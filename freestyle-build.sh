437da66418996713e8a5c23be6b9981aa33fe9cd
jenniekibiri <josphatje@gmail.com>
2022-04-11 16:56:08 +0300

    add dockerhub cred to jenkins script

--- freestyle-build.sh
+++ freestyle-build.sh
@@ -2,4 +2,6 @@ npm --version
 npm install
 npm test
 npm run build
-
+docker build -t jennykibiri/freestyle-jenkins-node-app .
+docker login -u $USERNAME -p $password
+docker push jennykibiri/freestyle-jenkins-node-app
